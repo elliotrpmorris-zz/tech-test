16b170099fa2b9d9d4683ec78eda043613adb823
Elliot <elliotmorris115@gmail.com>
2019-01-30 20:55:42 +0000

    got data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+//import axios from 'axios'
 import styled from 'styled-components';
 import LiveEvents from './components/LiveEvents'
 
@@ -8,31 +8,23 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      username: ''
     }
     /* Api stuff */
     // axios.get('https://randomuser.me/api/')
     //   .then(response => this.setState({ username: response.data.results[0].name.first }))
     //.then(response => console.log(response.data.results[0].name.first))
-    /* Websocket API */
-    // const w = new WebSocket("ws://localhost:8889");
-    // w.addEventListener("message", m => console.log(JSON.parse(m.data)));
-    // w.onopen = () => w.send(JSON.stringify({ type: "getLiveEvents", primaryMarkets: false }));
   }
   mounted
   render() {
-    const Title = styled.h1`
-      font-size: 1.5em;
-      text-align: center;
-      color: palevioletred;
+    const Container = styled.div`
+      max-width: 1170px;
+      margin: 0 auto;
     `;
 
     return (
-      <div className='button__container'>
-        <button className='button'>Click Me</button>
-        <Title>{this.state.username}</Title>
+      <Container>
         <LiveEvents />
-      </div>
+      </Container>
     )
   }
 }
--- src/components/LiveEvents.js
+++ src/components/LiveEvents.js
@@ -1,32 +1,60 @@
 import React, { Component } from 'react'
-//import styled from 'styled-components';
-//import Event from './Event'
+import styled from 'styled-components';
 
 class LiveEvents extends Component {
 	constructor() {
 		super()
 		this.state = {
-			liveEvents: '',
+			liveEvents: [],
 		}
+	}
+	getEvents(){
+		
 	}
 	componentDidMount() {
 		const w = new WebSocket("ws://localhost:8889");
-		w.onopen = () => {
-			console.log('Opened connection');
-
-			// send data to the server
-			w.addEventListener("message", e => (console.log()));
-			w.send(JSON.stringify({ type: "getLiveEvents", primaryMarkets: false }));
-		}
-		w.onmessage = (event) => {
-			this.setState({ liveEvents: JSON.parse(event.data) })
-			console.log(this.state.liveEvents);
-		}
+		w.onopen = () => {	
+			//w.addEventListener("message", e => (console.log()));
+			w.onmessage = (event) => {
+				this.setState({ liveEvents: JSON.parse(event.data) })		
+			}	
+		}	
+		setInterval( _ =>{
+			w.send(JSON.stringify({ type: "getLiveEvents", primaryMarkets: true }));
+		},2000 )
 	}
 
 	render() {
+		 const Table = styled.div`
+     	display: table;
+			width: 100%;
+		`;
+		const TableBody = styled.div`
+			display: table-row-group;
+		`;
+		const TableRow = styled.div`
+			display: table-row;
+		`;
+		const TableCell = styled.div`
+			border: 1px solid #999999;
+			display: table-cell;
+			padding: 10px;
+		`;
+		if(this.state.liveEvents.data && Array.isArray(this.state.liveEvents.data)){
+			console.log(this.state.liveEvents.data);
+			var eventList = this.state.liveEvents.data.map(function(e){
+				return <TableCell>{e.name}</TableCell>;
+			})
+		}
+
 		return (
-			<div></div>
+			<div>
+			<Table>
+				<TableBody>
+					{eventList}
+				</TableBody>
+			</Table>	
+		</div>
 		)
 	}
 }
