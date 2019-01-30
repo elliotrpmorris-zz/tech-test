b901cc27b8426f0d3c40739912be7c5e1d65f0c2
elliotm <elliot.morris@homeagency.co.uk>
2019-01-30 14:03:55 +0000

    styled components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import styled from 'styled-components';
 import './App.css'
+
 class App extends Component {
+
   constructor() {
     super()
     this.state = {
       username: ''
     }
+    axios.get('https://randomuser.me/api/')
+      .then(response => this.setState({ username: response.data.results[0].name.first }))
+    //.then(response => console.log(response.data.results[0].name.first))
   }
+  mounted
   render() {
+    const Title = styled.h1`
+      font-size: 1.5em;
+      text-align: center;
+      color: palevioletred;
+    `;
+
     return (
       <div className='button__container'>
         <button className='button'>Click Me</button>
+        <Title>{this.state.username}</Title>
       </div>
     )
   }
