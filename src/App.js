0741521c2fe55535aeae49e11f6fcd382ab9c462
elliotm <elliot.morris@homeagency.co.uk>
2019-01-30 13:25:51 +0000

    Create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './App.css'
+class App extends Component {
+  render() {
+    return (
+      <div className='button__container'>
+        <button className='button'>Click Me</button>
+      </div>
+    )
+  }
+}
+export default App
