580ac4ea54906423516176fb70eac3efd69b8814
elliotm <elliot.morris@homeagency.co.uk>
2019-01-30 13:40:34 +0000

    add axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css'
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: ''
+    }
+  }
   render() {
     return (
       <div className='button__container'>
