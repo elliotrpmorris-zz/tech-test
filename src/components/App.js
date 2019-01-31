fb55fe470da32d9dd898c5e95d9e626711ded802
Elliot <elliotmorris115@gmail.com>
2019-01-31 18:43:49 +0000

    added link styles

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,39 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components';
 
 const StyledLink = styled(Link)`
-  color: palevioletred;
+  color: black;
   font-weight: bold;
   padding: 10px;
+  margin: 5px;
+  text-decoration: none;
+  display: inline-block;
+  &:after {
+    content: '';
+    display: block;
+    width: 0;
+    height: 2px;
+    background: black;
+    transition: width .3s;
+  }
+  &:hover:after {
+    width: 100%;
+  }
+  &:hover{
+    font-weight: bold;
+  }
 `;
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 1170px;
+  padding: 10px;
+`;
+
 const App = () => (
-  <div>
+  <Container>
     <StyledLink to='/live-events'> View Live Events</StyledLink>
     <StyledLink to='/'>Home</StyledLink>
     <Main />
-  </div>
+  </Container>
 )
 export default App
